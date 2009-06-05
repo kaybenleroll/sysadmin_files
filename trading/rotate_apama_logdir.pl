5a8746858f739d830a55c87077a7e14ea2b6e5c6
Russell Clark <rclark@jacobsecurities.com>
2009-06-05 13:21:06 +0000

    The updated version of rotate_apama_logdir.pl
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@2 f22ea994-e202-4195-b786-b54d0d3ddba0

--- trading/rotate_apama_logdir.pl
+++ trading/rotate_apama_logdir.pl
@@ -4,21 +4,35 @@ use strict;
 use warnings;
 use diagnostics;
 
-my $logdir        = "/cygdrive/c/users/john/documents/perl/test";
-my $storagedir    = "/cygdrive/c/users/john/documents";
+use Getopt::Long;
+use File::Copy;
+use File::Path;
+use Archive::Tar;
+
+my $logdir = undef;
+my $storagedir = undef;
 my $t_stamp       = undef;
 
-system("cd $logdir");#move to directory to be compressed
+GetOptions('logdir=s'     => \$logdir,
+           'storagedir=s' => \$storagedir,
+           'timestamp=s' => \$t_stamp);
 
-$t_stamp = `date`; #load $t_stamp with the date and time
 
-$t_stamp =~ s/\://g; # remove all colons from time stamp string 
-$t_stamp =~ s/\s+//g; # remove all white sapces from time stamp string 
-#$t_stamp =~ s/ /_/g;  # replaces all white space with underscores 
+chdir "$logdir";
+
+if ($t_stamp = undef){
+    $t_stamp = `date`; #load $t_stamp with the date and time
+    $t_stamp =~ s/\://g; # remove all colons from time stamp string 
+    $t_stamp =~ s/\s+//g; # remove all white sapces from time stamp string 
+    #$t_stamp =~ s/ /_/g;  # replaces all white space with underscores
+}
 
 system("tar cjf apamalogs_${t_stamp}.tar.bz2 . "); #create compressed file
-
-system("mv apamalogs_${t_stamp}.tar.bz2 $storagedir"); #move compressed file to storage location
-
-system("rm -r $logdir"); #remove the directory that had just been compressed
-system("mkdir $logdir"); #recreate the directory that had just been deleted
+#@subdir = `ls -R`;
+#create_archive("$logdir"."apamalogs_${t_stamp}.tar.bz2",0,glob("*"));
+ 
+move("apamalogs_${t_stamp}.tar.bz2","$storagedir"); #move compressed file to storage location
+ 
+rmtree("$logdir"); #remove the directory that had just been compressed
+ 
+mkpath("$logdir"); #recreate the directory that had just been deleted
