a682b27610682c218d265320dd4da61796174755
Russell Clark <rclark@jacobsecurities.com>
2009-07-28 19:44:19 +0000

    Adding rollup_torc and updated script code
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@9 f22ea994-e202-4195-b786-b54d0d3ddba0

---
+++ trading/rollup_torc.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+my %positions;
+my $junk = <>;
+my $junk = <>;
+
+while(my $line = <>) {
+    
+    if ($line =~ /.*4PABCRF(.*).*/){
+
+    my @data = split(",", $1);
+
+    my $orderid   = $data[0];
+    my $symbol    = $data[8];
+    my $side      = $data[6];
+    my $executed  = $data[4];
+    my $price     = $data[5];
+
+
+    $symbol =~ s/"//g;
+    
+    if ($side eq "B"){
+        $side = "BUY";
+    }
+    elsif ($side eq "S"){
+        $side = "SELL";
+    }
+
+    my $venue;
+
+    my $printprice;
+
+    $printprice = sprintf("%4.2f", $price);
+    
+
+    print "US,$symbol,$side,$executed,0,$printprice\n";
+    }
+}
+
+exit(0);
+
+print "\n\nSummary:\n\n";
+
--- trading/script.pl
+++ trading/script.pl
@@ -5,266 +5,145 @@ use warnings;
 
 use Text::CSV;
 use Getopt::Long;
+use Date::Manip;
 use DBI;
 
-while($line = <>) {
+my $count = 0;
+my $count2 = 0;
+my $avg;
+my $Symbol;
+my $bids;
+my $asks;
+my $bidquantities;
+my $askquantities;
+my $time;
+my $BidSize;
+my $Bid;
+my $AskSize;
+my $Ask;
+my $number;
+my $filename;
+my @data;
+
+my $tradePrice;
+my $tradeQuantity;
+my $cumulativevalue;
+my $cumulativevolume;
+my $TradeCount;
+my $TradeDate;
+my $TradeCondition;
+my $TradeTime;
+my $CumulativePrice;
+my $LastReportedTradeSize;
+my $LastReportedTradeTime;
+my $TradeExchange;  
+
+my $date = "today";
+my $tstamp = undef;
+
+$date = UnixDate(ParseDate($date), "%Y/%m/%d");
+$tstamp = UnixDate(ParseDate($date), "%y%m%d");
+
+GetOptions('date=s'       => \$date,
+           'filename=s'   => \$filename);
+
+$filename = "dump_${tstamp}";
+open(FILE, ">$filename.sim");
+print FILE "#\n# <Timezone=US/Eastern>\n#\n";
+
+while(my $line = <>) {
     
-    if ($line =~ /.*ActivHeartbeatRequest\((.*)\)/){
-    	string $requestId;
-	dictionary<integer,float> timeStamp;
+    if ($count == 75000){
+        $count = 0;
+        close(FILE);
+        $count2 = $count2 + 1;
+        $number = sprintf ("%03d", $count2);
+        $filename = "dump_${tstamp}_${number}";
+        open(FILE, ">$filename.sim");    
+        print FILE "#\n# <Timezone=US/Eastern>\n#\n";        
+    }
+
+    if ($line =~ /.*ActivMarketBBODepth\((.*)\)/){
+
         @data = split(",",$1);
+
+        if ($#data >= 12){
       
-        $requestId              = $data[0];        
+            $Symbol         = $data[0];
+	    $bids           = $data[1];
+            #$bids           =~ s/[//g;
+            #$bids           =~ s/]//g;
+	    $asks           = $data[2];
+            #$asks           =~ s/[//g;
+            #$asks           =~ s/]//g;
+            #$avg            = ($bids + $asks)/2;
+	    $bidquantities  = $data[3];
+	    $askquantities  = $data[4];
+            $time           = $data[6];
+            $time           =~ /(\d{10}.\d)(\d{5})/;
+            $time           = $1;
+            $BidSize        = $data[10];
+            #$BidSize        =~ s/{//g;
+            $Bid            = $data[11];
+            $AskSize        = $data[12];
+            $Ask            = $data[13];
+            #$Ask            =~ s/})//g;
 
-        print FILE "";
-   
-    }
-
-    elsif ($line =~ /.*ActivHeartbeatResponse\((.*)\)/){
-    	string $requestId;
-	integer $HeartbeatStatus;
-	dictionary<integer,float> timeStamp;
-        @data = split(",",$1);
-      
-        $requestId              = $data[0];
-        $HeartbeatStatus        = $data[1];
-
-   
-    }
-
-    elsif ($line =~ /.*ActivConnectRequest\((.*)\)/){
-    	string $logonRequestId;
-	string $activServiceId;
-	string $userLogin;
-	string $password;
-	dictionary<integer,float> timeStamp;
-        @data = split(",",$1);
-      
-        $symbol              = $data[0];        
-    	$logonRequestId      = $data[1];
-	$activServiceId      = $data[2];
-	$userLogin           = $data[3];
-	$password            = $data[4];
-
-   
-    }
-
-    elsif ($line =~ /.*ActivConnectReply\((.*)\)/){
-    	string $logonRequestId;
-	string $activServiceId;
-	string $userLogin;
-	string $SessionToken;
-	integer $statusCode;
-	string $statusMsg;
-	dictionary<integer,float> timeStamp;
-	dictionary<string,string> __payload;
-        
-        @data = split(",",$1);
-      
-        $symbol             = $data[0];        
-    	$logonRequestId     = $data[1];
-	$activServiceId     = $data[2];
-	$userLogin          = $data[3];
-	$SessionToken       = $data[4];
-	$statusCode         = $data[5];
-	$statusMsg          = $data[6];
-
-   
-    }
-
-    elsif ($line =~ /.*ActivSubscribeRequest\((.*)\)/){
-    	string $subscribeRequestId;
-	string $subscribeType;
-	string $SessionToken;
-	string $Symbol;
-	boolean $isBBO10;
-	dictionary<integer,float> timeStamp;
-        @data = split(",",$1);
-      
-        $symbol              = $data[0];        
-    	$subscribeRequestId  = $data[1];
-	$subscribeType       = $data[2];
-	$SessionToken        = $data[3];
-	$Symbol              = $data[4];
-	$isBBO10             = $data[5];
-
-   
-    }
-
-    elsif ($line =~ /.*ActivSubscribeResponse\((.*)\)/){
-    	string $subscribeRequestId;
-	string $subscribeType;
-	string $SessionToken;
-	string $Symbol;
-	string $status;
-	string $statusMsg;
-	dictionary<integer,float> timeStamp;
-	dictionary<string,string> __payload;
-        @data = split(",",$1);
-      
-    	$subscribeRequestId   = $data[0];
-	$subscribeType        = $data[1];
-	$SessionToken         = $data[2];
-	$Symbol               = $data[3];
-	$status               = $data[4];
-	$statusMsg            = $data[5];        
-
-   
-    }
-
-    elsif ($line =~ /.*ActivUnsubscribeRequest\((.*)\)/){
-    	string $unsubscribeRequestId;
-	string $unsubscribeType;
-	string $SessionToken;
-	string $Symbol;
-	boolean $isBBO10;
-	dictionary<integer,float> timeStamp;
-        @data = split(",",$1);
-      
-        $unsubscribeRequestId = $data[0];
-	$unsubscribeType      = $data[1];
-	$SessionToken         = $data[2];
-	$Symbol               = $data[3];
-	$isBBO10              = $data[4];
-
-   
-    }
-
-    elsif ($line =~ /.*ActivUnsubscribeResponse\((.*)\)/){
-    	string $unsubscribeRequestId;
-	string $unsubscribeType;
-	string $SessionToken;
-	string $Symbol;
-	string $status;
-	string $statusMsg;
-	dictionary<integer,float> timeStamp;
-	dictionary<string,string> __payload;
-        @data = split(",",$1);
-                  
-    	$unsubscribeRequestId = $data[0];
-	$unsubscribeType      = $data[1];
-	$SessionToken         = $data[2];
-	$Symbol               = $data[3];
-	$status               = $data[4];
-	$statusMsg            = $data[5];
-
-   
-    }
-
-    elsif ($line =~ /.*ActivMarketDepth\((.*)\)/){
-    	string $Symbol;
-	sequence < float > $bids;
-	sequence < float > $asks;
-	sequence < integer > $bidquantities;
-	sequence < integer > $askquantities;
-	dictionary<integer,float> timeStamp;
-	dictionary<string,string> __payload;
-        @data = split(",",$1);
+            $count          = $count + 1;
             
-    	$Symbol         = $data[0];
-	$bids           = $data[1];
-	$asks           = $data[2];
-	$bidquantities  = $data[3];
-	$askquantities  = $data[4];
-
-   
-    }
-
-    elsif ($line =~ /.*ActivMarketBBODepth\((.*)\)/){
-    	string $Symbol;
-	sequence < float > $bids;
-	sequence < float > $asks;
-	sequence < integer > $bidquantities;
-	sequence < integer > $askquantities;
-	dictionary<integer,float> timeStamp;
-	dictionary<string,string> __payload;
-        @data = split(",",$1);
-      
-    	$Symbol         = $data[0];
-	$bids           = $data[1];
-	$asks           = $data[2];
-	$bidquantities  = $data[3];
-	$askquantities  = $data[4];        
-
-        print FILE "${time}00,internal,com.apama.marketdata.Depth("$Symbol",$bids,$asks,[33.585],$bidquantities,$askquantities,{"Ask":"33.59","AskSize":"6","Bid":"33.58","BidSize":"16","requestType":"-8"})";
-   
-    }
-
-    elsif ($line =~ /.*ActivMarketTick\((.*)\)/){
-    	string $Symbol;
-	float $tradePrice;
-	integer $tradeQuantity;
-	dictionary<integer,float> timeStamp;
-	dictionary<string,string> __payload;
-        @data = split(",",$1);
-      
-       
-    	$Symbol          = $data[0];
-	$tradePrice      = $data[1];
-	$tradeQuantity   = $data[2];
+            print FILE "${time}00,internal,com.apama.marketdata.Depth($Symbol,$bids,$asks,[NUMBER],$bidquantities,$askquantities,{$Ask,$AskSize,$Bid,$BidSize,\"requestType\":\"-8\"})\n";
+            }
         
-        $symbol =~ s/"//g;
-        $side   =~ s/"//g;
+        else{
 
-   
+            $Symbol         = $data[0];
+	    $bids           = $data[1];
+            #$bids           =~ s/[//g;
+            #$bids           =~ s/]//g;
+	    $asks           = $data[2];
+            #$asks           =~ s/[//g;
+            #$asks           =~ s/]//g;
+            #$avg            = ($bids + $asks)/2;
+	    $bidquantities  = $data[3];
+	    $askquantities  = $data[4];
+            $time           = $data[6];
+            $time           =~ /(\d{10}.\d)(\d{5})/;
+            $time                  = $1;
+            $AskSize        = $data[9];
+            $Ask            = $data[11];
+            #$Ask            =~ s/})//g;
+
+            $count          = $count + 1;
+
+            print FILE "${time}00,internal,com.apama.marketdata.Depth($Symbol,$bids,$asks,[NUMBER],$bidquantities,$askquantities,{$Ask,$AskSize,\"requestType\":\"-8\"})\n";
+        }
     }
+    elsif ($line =~ /.*ActivMarketTick\((.*)\)/){
 
-    elsif ($line =~ /.*ActivDisconnectRequest\((.*)\)/){
-    	string $logoffRequestId;
-	string $activServiceId;
-	string $userLogin;
-	string $SessionToken;
-	dictionary<integer,float> timeStamp;
-        @data = split(",",$1);
-              
-    	$logoffRequestId = $data[0];
-	$activServiceId  = $data[1];
-	$userLogin       = $data[2];
-	$SessionToken    = $data[3];
-
-        $symbol =~ s/"//g;
-        $side   =~ s/"//g;
-
-   
-    }
-
-    elsif ($line =~ /.*ActivDisconnectReply\((.*)\)/){
-    	string $logoffRequestId;
-	string $activServiceId;
-	string $userLogin;
-	string $SessionToken;
-	integer $statusCode;
-	dictionary<integer,float> timeStamp;
-	dictionary<string,string> __payload;
-        @data = split(",",$1);
-              
-    	string $logoffRequestId = $data[0];
-	string $activServiceId  = $data[1];
-	string $userLogin       = $data[2];
-	string $SessionToken    = $data[3];
-	integer $statusCode     = $data[4];
-
-        $symbol =~ s/"//g;
-        $side   =~ s/"//g;
-
-   
-    }
-
-    elsif ($line =~ /.*ActivContentGatewayConnectionDown\((.*)\)/){
-    	string $activServiceId;
-	string $userLogin;
-	sequence < string > $SessionTokens;
-	dictionary<integer,float> timeStamp;
-	dictionary<string,string> __payload;
         @data = split(",",$1);
              
-    	$activServiceId  = $data[0];
-	$userLogin       = $data[1];
-	$SessionTokens   = $data[2];
-        
-        $symbol =~ s/"//g;
-        $side   =~ s/"//g;
+    	$Symbol                = $data[0];
+	$tradePrice            = $data[1];
+	$tradeQuantity         = $data[2];
+        $time                  = $data[4];        
+        $time                  =~ /(\d{10}.\d)(\d{5})/;
+        $time                  = $1;
+        $cumulativevalue       = $data[7];
+        #$cumulativevalue       =~ s/{//g;        
+        $cumulativevolume      = $data[8];
+        $TradeCount            = $data[9];
+        $TradeDate             = $data[10];
+        $TradeCondition        = $data[11];
+        $TradeTime             = $data[12];
+        $CumulativePrice       = $data[13];
+        $LastReportedTradeSize = $data[14];
+        $LastReportedTradeTime = $data[15];
+        $TradeExchange         = $data[16];        
 
-   
+        $count                 = $count + 1;
+
+        print FILE "${time}00,internal,com.apama.marketdata.Tick($Symbol,$tradePrice,$tradeQuantity,{$CumulativePrice,$cumulativevalue,$cumulativevolume,$LastReportedTradeSize,$LastReportedTradeTime,$TradeCondition,$TradeCount,$TradeDate,$TradeExchange,$TradeTime,\"requestType\":\"-7\"})\n";
+
     }
+
 }
