d7a1226106563b24e212dde2d5bdbca9e38031d5
Russell Clark <rclark@jacobsecurities.com>
2009-07-30 20:32:47 +0000

    Added volume and cash volume.
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@12 f22ea994-e202-4195-b786-b54d0d3ddba0

--- trading/calculate_pnl.pl
+++ trading/calculate_pnl.pl
@@ -5,6 +5,10 @@ use warnings;
 
 my $cad_cashflow = 0;
 my $usd_cashflow = 0;
+my $ca_volume=0;
+my $us_volume=0
+my $ca_cashvolume = 0;
+my $us_cashvolume = 0;
 
 my $cad_fx = 0;
 my $usd_fx = 0;
@@ -23,9 +27,13 @@ while(my $line = <>) {
     } elsif($venue eq "CA") {
 	$ca_positions{"$symbol"} += ($qty          * ($side eq "BUY" ?  1 : -1));
 	$cad_cashflow            += ($qty * $price * ($side eq "BUY" ? -1 :  1));
+	$ca_volume               += ($qty         );
+	$ca_cashvolume           += ($qty * $price);
     } elsif($venue eq "US") {
 	$us_positions{"$symbol"} += ($qty          * ($side eq "BUY" ?  1 : -1));
 	$usd_cashflow            += ($qty * $price * ($side eq "BUY" ? -1 :  1));
+	$us_volume               += ($qty         );
+	$us_cashvolume           += ($qty * $price); 
     } else {
 	print "Error with line $line";
     }
@@ -50,4 +58,9 @@ print "\n";
 print "USD Cashflow: " . sprintf("% 10.2f", $usd_cashflow) . "\n";
 print "USD FX:       " . sprintf("% 10.2f", $usd_fx) . "\n";
 print "USD Total:    " . sprintf("% 10.2f", $usd_fx + $usd_cashflow) . "\n";
+print "\n";
+print "CA Cash Volume: " . sprintf("% 10.2f", $ca_cashvolume) . "\n";
+print "CA Volume       " . sprintf("% 10.2f", $ca_volume) . "\n";
+print "US Cash Volume: " . sprintf("% 10.2f", $us_cashvolume) . "\n";
+print "US Volume       " . sprintf("% 10.2f", $us_volume) . "\n";
 
