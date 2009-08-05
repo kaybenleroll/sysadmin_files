de5ad5350b399b62d1b4a5c1f1125389beb0a5d4
Russell Clark <rclark@jacobsecurities.com>
2009-08-05 18:42:56 +0000

    Updated generate_tradefile.pl to add cumlpnl creation. Commented out ftping in cron_event.pl and added diff check.
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@16 f22ea994-e202-4195-b786-b54d0d3ddba0

--- trading/cron_event.pl
+++ trading/cron_event.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 use Getopt::Long;
+use File::Copy;
 use Net::FTP;
 use Date::Manip;
 
@@ -13,7 +14,7 @@ my $host     = '207.35.239.17';
 my $user     = 'F96FTP01';
 my $pass     = 'VLZWR3IBK1';
 
-my $filename = "/var/jsi/atalogs/ATA_correlator_${date}*";
+my $filename;
 
 GetOptions('filename=s'   => \$filename,
            'date=s'       => \$date,
@@ -24,6 +25,8 @@ GetOptions('filename=s'   => \$filename,
 $date        = UnixDate(ParseDate($date), "%Y%m%d");
 $tstamp      = UnixDate(ParseDate($date), "%m%d");
 
+$filename = "/var/jsi/atalogs/ATA_correlator_${date}*";
+
 system("cat $filename | grep OrderUpdate | perl process_fills.pl > trades_${date}.csv");
 
 system("cat $filename | grep OrderUpdate | perl process_fills.pl | grep -v HTC | perl calculate_pnl.pl > pnl_${date}.csv");
@@ -32,14 +35,17 @@ system("cat trades_*  |  grep -v HTC | perl calculate_pnl.pl > cumlpnl_${date}.c
 
 system("cat $filename | grep OrderUpdate | perl process_fills.pl | perl generate_dropfile.pl");
 
+move("F96TR${tstamp}1.csv", "/var/jsi/pensonfiles/");
+move("F96TR${tstamp}2.csv", "/var/jsi/pensonfiles/");
+
+system("cat /var/jsi/torcfiles/JacobSecurities_${date} | perl rollup_torc.pl | perl generate_dropfile.pl");
+
+system("diff F96TR${tstamp}2 /var/jsi/pensonfiles/F96TR${tstamp}2");
+
+system("rm F96TR${tstamp}2");
 
 #my $ftp_h = Net::FTP->new($host, Debug => 0);
 #$ftp_h->login($user, $pass);
 #$ftp_h->cwd("/eod");
 #$ftp_h->put("F96TR${tstamp}1.csv");
 #$ftp_h->put("F96TR${tstamp}2.csv");
-
-
-move("F96TR${tstamp}1.csv", "/var/jsi/pensonfiles/");
-move("F96TR${tstamp}2.csv", "/var/jsi/pensonfiles/");
-
--- trading/generate_tradefile.pl
+++ trading/generate_tradefile.pl
@@ -11,20 +11,28 @@ my $today;
 my $date;
 my $tstamp;
 my $err;
+my $some_dir = "/var/jsi/atalogs/";
+my @array;
+my $count = 0;
 
 $today  = UnixDate(ParseDate('today'), "%Y%m%d");
+$today  = DateCalc($today,"+ 1 days",\$err);
+$today  = UnixDate(ParseDate($today), "%Y%m%d");
 $date   = UnixDate(ParseDate('May 8th, 2009'), "%Y%m%d");
 $tstamp = UnixDate(ParseDate($date), "%m%d");
+
 
 while ($date != $today){
 
     if (-e "/var/jsi/apama/logfiles/apamalogs_${date}.tar.bz2") {
-        chdir "/var/jsi/atalogs";
-        system("tar xvjpf /var/jsi/apama/logfiles/apamalogs_${date}.tar.bz2 --wildcards ./*correlator_${date}* ");
-        chdir "/var/jsi/tradefile_pnl/test";
+        chdir "/var/jsi/tradefile_pnl";
         system("cat /var/jsi/atalogs/*correlator_${date}* | grep OrderUpdate | perl /var/jsi/tradefile_pnl/process_fills.pl > trades_${date}.csv");   
+        system("cat /var/jsi/tradefile_pnl/trades_${date}.csv | grep -v HTC | perl /var/jsi/tradefile_pnl/calculate_pnl.pl > pnl_${date}.csv");
+        $array[$count] = "/var/jsi/atalogs/*correlator_${date}* ";
+        $count = $count + 1;
+        system("cat @array | grep OrderUpdate | perl process_fills.pl | perl calculate_pnl.pl > cumlpnl_${date}.csv");
     }
-    
+
     $date   = DateCalc($date,"+ 1 days",\$err);
     $date   = UnixDate(ParseDate($date), "%Y%m%d");
     $tstamp = UnixDate(ParseDate($date), "%m%d");
