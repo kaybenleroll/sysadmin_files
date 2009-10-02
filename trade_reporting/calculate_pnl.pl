a8cc9192088c84117b761282c9311ac199202bf7
Mick Cooney <mcooney@jacobsecurities.com>
2009-10-02 23:18:42 +0000

    Continued reformatting the trade_reporting scripts to conform to standards
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@35 f22ea994-e202-4195-b786-b54d0d3ddba0

--- trade_reporting/calculate_pnl.pl
+++ trade_reporting/calculate_pnl.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-#Declaration and Initialization of Variables
+### Declaration and Initialization of Variables
 my $cad_cashflow = 0;
 my $usd_cashflow = 0;
 my $ca_volume = 0;
@@ -23,47 +23,47 @@ my %ca_positions;
 my %us_positions;
 
 
-#While statement that takes every line passed into the program through standard input and adds their volumes and premiums to their respective venue's variables
+### While statement that takes every line passed into the program through standard input and adds their volumes and premiums to their respective venue's variables
 while(my $line = <>) {
     my ($venue, $symbol, $side, $qty, $remain, $price) = split(",", $line);
 
     if($venue eq "CNX") {
-	$usd_fx                  += ($qty          * ($side eq "BUY" ?  1 : -1));
-	$cad_fx                  += ($qty * $price * ($side eq "BUY" ? -1 :  1));
+        $usd_fx                  += ($qty          * ($side eq "BUY" ?  1 : -1));
+        $cad_fx                  += ($qty * $price * ($side eq "BUY" ? -1 :  1));
         $exchange_rate            = ($price        +(($side eq "BUY" ? -1 :  1)*0.00025));
     } elsif($venue eq "CA") {
-	$ca_positions{"$symbol"} += ($qty          * ($side eq "BUY" ?  1 : -1));
-	$cad_cashflow            += ($qty * $price * ($side eq "BUY" ? -1 :  1));
-	$ca_volume               += ($qty         );
-	$ca_cashvolume           += ($qty * $price);
+        $ca_positions{"$symbol"} += ($qty          * ($side eq "BUY" ?  1 : -1));
+        $cad_cashflow            += ($qty * $price * ($side eq "BUY" ? -1 :  1));
+        $ca_volume               += ($qty         );
+        $ca_cashvolume           += ($qty * $price);
     } elsif($venue eq "US") {
-	$us_positions{"$symbol"} += ($qty          * ($side eq "BUY" ?  1 : -1));
-	$usd_cashflow            += ($qty * $price * ($side eq "BUY" ? -1 :  1));
-	$us_volume               += ($qty         );
-	$us_cashvolume           += ($qty * $price); 
+        $us_positions{"$symbol"} += ($qty          * ($side eq "BUY" ?  1 : -1));
+        $usd_cashflow            += ($qty * $price * ($side eq "BUY" ? -1 :  1));
+        $us_volume               += ($qty         );
+        $us_cashvolume           += ($qty * $price); 
     } else {
-	print "Error with line $line";
+        print "Error with line $line";
     }
 }
 
-# Prints out every canadian symbol and their respective positions
+### Prints out every canadian symbol and their respective positions
 foreach my $symbol (sort keys %ca_positions) {
     print $symbol . "," . $ca_positions{"$symbol"} . "\n"; 
 }
 
 print "\n";
 
-# Prints out every us symbol and their respective positions
+### Prints out every us symbol and their respective positions
 foreach my $symbol (sort keys %us_positions) {
     print $symbol . "," . $us_positions{"$symbol"} . "\n"; 
 }
 
 print "\n";
 
-# Open's the csv file InterlistedStocks and greats a hash with the key the canadian symbol and the value the us symbol
+### Open's the csv file InterlistedStocks and greats a hash with the key the canadian symbol and the value the us symbol
 print "Not flat:\n";
 
-open(FILE, "InterlistedStocks.csv") || die ("Could not open file!");
+open(FILE, "InterlistedStocks.csv") || die ("Could not open file InterlistedStocks.csv");
 
 while(my $line = <FILE> ) {
     my ($casymbol, $ussymbol) = split(",", $line);
@@ -76,8 +76,8 @@ while(my $line = <FILE> ) {
 
 close(FILE);
 
-#For every symbol, it adds it's canadian and us equivalents together to calculate their overall position
-#If position is not 0, program outputs the position
+### For every symbol, it adds it's canadian and us equivalents together to calculate their overall position
+### If position is not 0, program outputs the position
 foreach my $symbol (sort keys %ca_positions) {
     if (!$us_positions{$interlistedhash{$symbol}}){
         $us_positions{$interlistedhash{$symbol}} = 0;
@@ -89,15 +89,15 @@ foreach my $symbol (sort keys %ca_positions) {
     }
 }
 
-#If all stocks are flat prints out None
+### If all stocks are flat prints out None
 if ($count == 0) {
     print "None";
 }
 
-#Calculates unhedged pnl
+### Calculates unhedged pnl
 $unhedged_pnl = $usd_cashflow * $exchange_rate + $cad_cashflow;
 
-#Prints out US and Canadian Cash Volume, Volume, Cashflow, FX and Total as well as the Unhedged PNL
+### Prints out US and Canadian Cash Volume, Volume, Cashflow, FX and Total as well as the Unhedged PNL
 print "\n\n";
 print "CAD Cashflow: " . sprintf("% 10.2f", $cad_cashflow) . "\n";
 print "CAD FX:       " . sprintf("% 10.2f", $cad_fx) . "\n";
@@ -113,5 +113,4 @@ print "US Cash Volume: " . sprintf("% 10.2f", $us_cashvolume) . "\n";
 print "US Volume       " . sprintf("% 10.2f", $us_volume) . "\n";
 print "\n";
 print "Unhedged PNL: " . sprintf("%10.2f", $unhedged_pnl) . "\n";
-
 
