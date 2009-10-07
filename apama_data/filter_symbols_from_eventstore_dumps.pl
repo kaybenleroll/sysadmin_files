0449452c7c6b66c5b2116854f699c6198735b9c2
Mick Cooney <mcooney@jacobsecurities.com>
2009-10-07 00:02:32 +0000

    Started working on the Penson reports automation
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@39 f22ea994-e202-4195-b786-b54d0d3ddba0

--- apama_data/filter_symbols_from_eventstore_dumps.pl
+++ apama_data/filter_symbols_from_eventstore_dumps.pl
@@ -6,7 +6,7 @@ use diagnostics;
 
 use Getopt::Long;
 
-my $symbol_list = "ABX.TO,ABX.,RY.TO,RY.,G.TO,GG.,SU.TO,SU.,XIU.TO,CNQ.TO,CNQ.,USD/CAD";
+my $symbol_list = "ABX.TO,ABX.,RY.TO,RY.,G.TO,GG.,SU.TO,SU.,XIU.TO,XEG.TO,XIT.TO,SPY.,QQQQ.,CNQ.TO,CNQ.,USD/CAD";
 
 
 my @symbols = split(",", $symbol_list);
---
+++ trade_reporting/email_penson_files.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use File::Copy;
+use Net::FTP;
+use Date::Manip; 
+
+my $date         = 'today';
+my $tstamp       = undef;
+my $host         = '207.35.239.17';
+my $user         = 'F96FTP01';
+my $pass         = 'VLZWR3IBK1';
+my $download_dir = '/var/jsi/pensonreports';
+my $email_addr   = 'testgroup@jacobsecurities.com';
+
+
+GetOptions('date=s'         => \$date,
+           'email_addr=s'   => \$email_addr,
+           'download_dir=s' => \$download_dir);
+
+$tstamp      = UnixDate(DateCalc(ParseDate($date),"-1 business day"), "%Y-%m-%d");
+
+print "Downloading timestamp is $tstamp\n";
+
+mkdir("$download_dir/$tstamp");
+chdir("$download_dir/$tstamp");
+
+
+#my $ftp_h = Net::FTP->new($host, Debug => 0);
+#$ftp_h->login($user, $pass);
+
+#$ftp_h->cwd("/home/F96FTP01/Reports");
+
+#my @file_list = $ftp_h->ls();
+
+#foreach my $file (@file_list) {
+#    print $file . "\n" if $file !~ /\./;
+#}
