19837e9664a6e9155696962c857663c5434cedb3
Mick Cooney <mcooney@jacobsecurities.com>
2009-10-15 20:02:46 +0000

    Added a symbol PNL calculation to the perform_compression script
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@45 f22ea994-e202-4195-b786-b54d0d3ddba0

--- trade_reporting/perform_compression.pl
+++ trade_reporting/perform_compression.pl
@@ -5,6 +5,7 @@ use warnings;
 
 
 my %positions;
+my %symbol_pnl;
 
 while(my $line = <>) {
     my ($venue, $symbol, $side, $qty, $remain, $price) = split(",", $line);
@@ -12,8 +13,10 @@ while(my $line = <>) {
     $positions{"$venue"}{"$symbol"}{"$side"}{"count"}    += 1;
     $positions{"$venue"}{"$symbol"}{"$side"}{"quantity"} += $qty;
     $positions{"$venue"}{"$symbol"}{"$side"}{"money"}    += $qty * $price;
-}
     
+    $symbol_pnl{"$venue"}{"$symbol"} += ($side eq "SELL" ? 1 : -1) * ($qty * $price);
+}
+
 
 
 foreach my $venue (sort keys %positions) {
@@ -22,19 +25,27 @@ foreach my $venue (sort keys %positions) {
     next if $venue eq "CNX";
 
     foreach my $symbol (sort keys %{ $positions{"$venue"} }) {
-	foreach my $side (sort keys %{ $positions{"$venue"}{"$symbol"} }) {
-	    my $fills     = $positions{"$venue"}{"$symbol"}{"$side"}{"count"};
-	    my $shares    = $positions{"$venue"}{"$symbol"}{"$side"}{"quantity"};
-	    my $avg_price = sprintf("%0.6f", $positions{"$venue"}{"$symbol"}{"$side"}{"money"} / $shares);
+        foreach my $side (sort keys %{ $positions{"$venue"}{"$symbol"} }) {
+            my $fills     = $positions{"$venue"}{"$symbol"}{"$side"}{"count"};
+            my $shares    = $positions{"$venue"}{"$symbol"}{"$side"}{"quantity"};
+            my $avg_price = sprintf("%0.6f", $positions{"$venue"}{"$symbol"}{"$side"}{"money"} / $shares);
 
-	    print "$venue,$symbol,$side,$shares,$avg_price\n";
+            print "$venue,$symbol,$side,$shares,$avg_price\n";
 
-	    next if $venue =~ /CNX/;
+            next if $venue =~ /CNX/;
 
-	    $total_currency += $positions{"$venue"}{"$symbol"}{"$side"}{"money"} * ($side eq "BUY" ? -1 : 1);
-	}
+            $total_currency += $positions{"$venue"}{"$symbol"}{"$side"}{"money"} * ($side eq "BUY" ? -1 : 1);
+        }
     }
 
     print "\n$venue,${total_currency}\n\n" unless $venue =~ /CNX/;
 }
 
+
+foreach my $venue (sort keys %positions) {
+    foreach my $symbol (sort keys %{ $positions{"$venue"} }) {
+        print "$venue,$symbol," . $symbol_pnl{"$venue"}{"$symbol"} . "\n";
+    }
+}
+        
+        
