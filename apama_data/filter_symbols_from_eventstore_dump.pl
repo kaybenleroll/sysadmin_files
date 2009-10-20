2e0b99d198bb36953f2243b8e26dbb55340876f5
Mick Cooney <mcooney@jacobsecurities.com>
2009-10-20 00:38:21 +0000

    Added the eventstore symbol filter script
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@50 f22ea994-e202-4195-b786-b54d0d3ddba0

---
+++ apama_data/filter_symbols_from_eventstore_dump.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use diagnostics;
+
+use Getopt::Long;
+
+my $symbol_list = "ABX.TO,ABX.,RY.TO,RY.,G.TO,GG.,SU.TO,SU.,XIU.TO,XEG.TO,XIT.TO,SPY.,QQQQ.,CNQ.TO,CNQ.,USD/CAD";
+
+print "# <Timezone=US/Eastern>\n";
+print "#\n";
+
+my @symbols = split(",", $symbol_list);
+
+my $regexp = "(\"" . join("\"|\"", @symbols) . "\")";
+
+while(my $line = <>) {
+    print $line if $line =~ /$regexp/;
+}
+
+exit(0);
