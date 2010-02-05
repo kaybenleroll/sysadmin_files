a0b57625c374d4414c60a148842806ba119b8dfa
Mick Cooney <mcooney@jacobsecurities.com>
2010-02-05 00:03:56 +0000

    Added some parsing to the ExecutionReport message
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@76 f22ea994-e202-4195-b786-b54d0d3ddba0

--- trade_reporting/tsx_latency.pl
+++ trade_reporting/tsx_latency.pl
@@ -42,25 +42,25 @@ while(my $line = <>) {
         $symbol     =~ s/"//g;
 
         #Assigns the information to a hash with the orderid as a key so that if an order is repeated it will not be printed out twice
-        $orderdata{"$fixorderid"}{'fixorderid'}     = $fixorderid;
-        $orderdata{"$fixorderid"}{'symbol'}         = $symbol;
-        $orderdata{"$fixorderid"}{'action'}         = $action;
-        $orderdata{"$fixorderid"}{'timestamp'}      = $timestamp;
-        $orderdata{"$fixorderid"}{'epochtime'}      = $epoch;
+        $orderdata{"$fixorderid"}{"$action"}{'fixorderid'} = $fixorderid;
+        $orderdata{"$fixorderid"}{"$action"}{'symbol'}     = $symbol;
+        $orderdata{"$fixorderid"}{"$action"}{'timestamp'}  = $timestamp;
+        $orderdata{"$fixorderid"}{"$action"}{'epochtime'}  = $epoch;
 
-        $orderdata{"$fixorderid"}{'value'}          = sprintf("ERROR:  %s,%s,%s,,%s,%6.4f\n",
-                                                              $orderdata{"$fixorderid"}{'symbol'},
-                                                              $orderdata{"$fixorderid"}{'action'},
-                                                              $orderdata{"$fixorderid"}{'timestamp'},
-                                                              $orderdata{"$fixorderid"}{'fixorderid'},
-                                                              $orderdata{"$fixorderid"}{'epochtime'});
+        my $value = sprintf("ERROR:  %s,%s,%s,,%s,%6.4f\n", $orderdata{"$fixorderid"}{"$action"}{'symbol'},
+                                                            $action,
+                                                            $orderdata{"$fixorderid"}{"$action"}{'timestamp'},
+                                                            $orderdata{"$fixorderid"}{"$action"}{'fixorderid'},
+                                                            $orderdata{"$fixorderid"}{"$action"}{'epochtime'});
+                                                         
+        $orderdata{"$fixorderid"}{'$action'}{'value'} = $value;
 
     } elsif ($line =~ /.*com.apama.fix.OrderCancelReplaceRequest\((.*)\)/){
         my @data = split(",", $1);
 
+        my $tsxorderid   = $data[2];
         my $fixorderid   = $data[4];
         my $symbol       = $data[7];
-        my $tsxorderid   = $data[2];
         my $action       = 'OrderCancelReplaceRequest';
 
         #Pulls timestamp from line and assigns it to variable
@@ -78,28 +78,47 @@ while(my $line = <>) {
         $symbol     =~ s/"//g;  
         $tsxorderid =~ s/"//g;
 
-        $orderdata{"$fixorderid"}{'fixorderid'}     = $fixorderid;
-        $orderdata{"$fixorderid"}{'tsxorderid'}     = $tsxorderid;
-        $orderdata{"$fixorderid"}{'symbol'}         = $symbol;
-        $orderdata{"$fixorderid"}{'action'}         = $action;
-        $orderdata{"$fixorderid"}{'timestamp'}      = $timestamp;
-        $orderdata{"$fixorderid"}{'epochtime'}      = $epoch;
+        $orderdata{"$fixorderid"}{"$action"}{'fixorderid'} = $fixorderid;
+        $orderdata{"$fixorderid"}{"$action"}{'tsxorderid'} = $tsxorderid;
+        $orderdata{"$fixorderid"}{"$action"}{'symbol'}     = $symbol;
+        $orderdata{"$fixorderid"}{"$action"}{'timestamp'}  = $timestamp;
+        $orderdata{"$fixorderid"}{"$action"}{'epochtime'}  = $epoch;
 
-        $orderdata{"$fixorderid"}{'value'}          = sprintf("ERROR:  %s,%s,%s,%s,%s,%6.4f\n",
-                                                              $orderdata{"$fixorderid"}{'symbol'},
-                                                              $orderdata{"$fixorderid"}{'action'},
-                                                              $orderdata{"$fixorderid"}{'timestamp'},
-                                                              $orderdata{"$fixorderid"}{'tsxorderid'},
-                                                              $orderdata{"$fixorderid"}{'fixorderid'},
-                                                              $orderdata{"$fixorderid"}{'epochtime'});
+        my $value = sprintf("ERROR:  %s,%s,%s,%s,%s,%6.4f\n", $orderdata{"$fixorderid"}{"$action"}{'symbol'},
+                                                              $action,
+                                                              $orderdata{"$fixorderid"}{"$action"}{'timestamp'},
+                                                              $orderdata{"$fixorderid"}{"$action"}{'tsxorderid'},
+                                                              $orderdata{"$fixorderid"}{"$action"}{'fixorderid'},
+                                                              $orderdata{"$fixorderid"}{"$action"}{'epochtime'});
+
+        $orderdata{"$fixorderid"}{"$action"}{'value'} = $value;
 
     } elsif ($line =~ /.*com.apama.fix.ExecutionReport\((.*)\)/){
         my @data = split(",", $1);
 
         my $tsxorderid   = $data[2];
         my $fixorderid   = $data[3];
+        my $execType     = $data[7];
+        my $ordStatus    = $data[8];
         my $symbol       = $data[10];
 
+        my $status = '';
+        my $action = '';
+
+        if($execType eq '0') {
+            $action = 'NewOrderSingle';
+        } elsif(($execType eq 'E') or ($execType eq '5')) {
+            $action = 'OrderCancelReplaceRequest';
+        }
+
+        if($ordStatus eq 'E') {
+            $status = 'Pending';            
+        } elsif($ordStatus eq '5') {
+            $status = 'Replaced';
+        } else {
+            $status = 'New';
+        }
+ 
         #Pulls timestamp from line and assigns it to variable
         $line =~ /^(.*?) WARN/;
         $timestamp    = $1;
@@ -115,28 +134,25 @@ while(my $line = <>) {
         $symbol     =~ s/"//g;
         $tsxorderid =~ s/"//g;
 
-        $orderdata{"$fixorderid"}{'latency'}    = $epoch - $orderdata{"$fixorderid"}{'epochtime'};
-        $orderdata{"$fixorderid"}{'tsxorderid'} = $orderdata{"$fixorderid"}{'tsxorderid'} ? $orderdata{"$fixorderid"}{'tsxorderid'} : '';
-        
-        $orderdata{"$fixorderid"}{'value'}   = sprintf("LATENCY: %s,%s,%s,%s,%s,%5.3f",
-                                                       $orderdata{"$fixorderid"}{'symbol'},
-                                                       $orderdata{"$fixorderid"}{'action'},
-                                                       $orderdata{"$fixorderid"}{'timestamp'},
-                                                       $orderdata{"$fixorderid"}{'tsxorderid'},
-                                                       $orderdata{"$fixorderid"}{'fixorderid'},
-                                                       $orderdata{"$fixorderid"}{'latency'});
-                                                       
+        $orderdata{"$fixorderid"}{"$action"}{'latency'}    = $epoch - $orderdata{"$fixorderid"}{"$action"}{'epochtime'};
+        $orderdata{"$fixorderid"}{"$action"}{'tsxorderid'} = $orderdata{"$fixorderid"}{"$action"}{'tsxorderid'} ? $orderdata{"$fixorderid"}{"$action"}{'tsxorderid'} : '';
+
+        my $value = sprintf("LATENCY: %s,%s,%s,%s,%s,%s,%5.3f", $orderdata{"$fixorderid"}{"$action"}{'symbol'},
+                                                                $action,
+                                                                $status,
+                                                                $orderdata{"$fixorderid"}{"$action"}{'timestamp'},
+                                                                $orderdata{"$fixorderid"}{"$action"}{'tsxorderid'},
+                                                                $orderdata{"$fixorderid"}{"$action"}{'fixorderid'},
+                                                                $orderdata{"$fixorderid"}{"$action"}{'latency'});
+
+        $orderdata{"$fixorderid"}{'value'} = $value;
+
         print $orderdata{"$fixorderid"}{'value'} . "\n";
-        
+
 #        delete $orderdata{"$fixorderid"};
     }
 
 }
-
-# Prints out every entry in the hash
-#foreach my $key (sort { $orderdata{$a}{'fixorderid'} cmp $orderdata{$b}{'fixorderid'} } keys %orderdata) {
-#    print $orderdata{"$key"}{'value'} . "\n";
-#}
 
 exit(0);
 
