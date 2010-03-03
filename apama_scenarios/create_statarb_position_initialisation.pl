557c03ada0e30f0ae118526725553cfd8b3618da
Mick Cooney <mcooney@jacobsecurities.com>
2010-03-03 15:57:47 +0000

    Added a small script to quickly create the position initialisation for StatArb scenario
    git-svn-id: file:///var/lib/svn/repository/sysadmin/trunk@89 f22ea994-e202-4195-b786-b54d0d3ddba0

---
+++ apama_scenarios/create_statarb_position_initialisation.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use diagnostics;
+
+while(my $line = <>) {
+    next unless $line =~ / (.*) TRADEHALT (.*)/;
+
+    my $scenario_label = $1;
+    my ($driver_symbol, $quoter_symbol) = split('-', $scenario_label);
+
+    my ($driver_pos, $driver_cash, $quoter_pos, $quoter_cash) = split(',', $2);
+
+    next unless (($driver_pos != 0) and ($quoter_pos != 0));
+
+    my $message_key = "STATARB-POSITIONS-${scenario_label}";
+
+    print "com.jacobsecurities.scenario.communication.DictionaryMessage(\"${message_key}\"," .
+          "\{" .
+          "\"${driver_symbol}-EXTPOS\":\"${driver_pos}\"," .
+          "\"${driver_symbol}-EXTCASH\":\"${driver_cash}\"," .
+          "\"${quoter_symbol}-EXTPOS\":\"${quoter_pos}\"," .
+          "\"${quoter_symbol}-EXTCASH\":\"${quoter_cash}\"," .
+          "\})\n";
+}
