30c95efa3ad6f9e13449448b38b5450b7045eb66
Mick Cooney <mcooney@jacobsecurities.com>
2010-04-27 21:50:02 -0400

    Corrected the directory path

--- cronjobs/start_ata_41.sh
+++ cronjobs/start_ata_41.sh
@@ -3,7 +3,7 @@
 APAMA_INSTALL_DIR=/opt/apama_4.1
 ATA_DIR=/apama/ATA111/Accelerators/AlgorithmicTradingAccelerator
 
-source ${APAMA_DIR}/bin/apama_env
+source ${APAMA_INSTALL_DIR}/bin/apama_env
 
 ${ATA_DIR}/project.sh start
 engine_send ${ATA_DIR}/src/jcsevents/SubscribeToData.evt
