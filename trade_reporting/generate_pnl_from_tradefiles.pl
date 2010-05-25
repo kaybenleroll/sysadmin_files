80380bb20748f37e8ace983bfdee0eb6f0f88e55
Russell Clark <russell@russell-desktop.(none)>
2010-05-25 11:27:01 -0400

    Finished generate pnl script. For net position value the last price traded at is used for calculation.

--- trade_reporting/generate_pnl_from_tradefiles.pl
+++ trade_reporting/generate_pnl_from_tradefiles.pl
@@ -3,6 +3,17 @@
 use strict;
 use warnings;
 
+my %venues;
+my %vlmtraded;
+my %dollarvlmtraded;
+my %vlmpassive;
+my %vlmactive;
+my %netpos;
+my %netposvalue;
+my %netcash;
+my %pnl;
+my %lastprice;
+
 
 while(my $line = <>) {
 
@@ -22,11 +33,43 @@ while(my $line = <>) {
     my $orderid      = $data[9];
     my $liquidity    = $data[10];
 
+    if (!$vlmtraded{$symbol}{$venue}) {
+        $venues{$symbol}{$venue} = 0;
+        $vlmtraded{$symbol}{$venue} = 0;
+        $dollarvlmtraded{$symbol}{$venue} = 0;
+        $vlmpassive{$symbol}{$venue} = 0;
+        $vlmactive{$symbol}{$venue} = 0;
+        $netpos{$symbol}{$venue} = 0;
+        $netposvalue{$symbol}{$venue} = 0;
+        $netcash{$symbol}{$venue} = 0;
+        $pnl{$symbol}{$venue} = 0;
+    }
 
+    $lastprice{$symbol}{$venue} = $printprice;
+    $vlmtraded{$symbol}{$venue} += $executed;
+    $dollarvlmtraded{$symbol}{$venue} += ($executed * $printprice);
 
-    print "$venue,$symbol,$vlmtraded,$dollarvlmtraded,$vlmpassive,$vlmactive,$netpos,$netposvalue,$netcash,$pnl\n";
+    if ($liquidity eq 'TPPNN'||$liquidity eq 'XPPNN' ) {
+        $vlmpassive{$symbol}{$venue} += $executed;
+    } else {
+        $vlmactive{$symbol}{$venue} += $executed;
+    }
 
+    if ($side eq 'BUY') {
+        $netpos{$symbol}{$venue} += $executed;     
+        $netcash{$symbol}{$venue} -= ($executed * $printprice);
+    } elsif ($side eq 'SELL') {
+        $netpos{$symbol}{$venue} -= $executed;
+        $netcash{$symbol}{$venue} += ($executed * $printprice);
+    }
+  
 }
 
-
+foreach my $symbol (sort keys %lastprice) {
+    foreach my $venue (sort keys %{$lastprice{$symbol}}) {
+        $netposvalue{$symbol}{$venue} = $netpos{$symbol}{$venue} * $lastprice{$symbol}{$venue};
+        $pnl{$symbol}{$venue} = $netposvalue{$symbol}{$venue} + $netcash{$symbol}{$venue};
+        print "$venue,$symbol,$vlmtraded{$symbol}{$venue},$dollarvlmtraded{$symbol}{$venue},$vlmpassive{$symbol}{$venue},$vlmactive{$symbol}{$venue},$netpos{$symbol}{$venue},$netposvalue{$symbol}{$venue},$netcash{$symbol}{$venue},$pnl{$symbol}{$venue}\n";
+    }
+}
 exit(0);
