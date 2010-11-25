417b42198b964530a55fa1850a66d067d41605a4
Mick Cooney <mcooney@kaybensoft.com>
2010-11-25 20:41:25 +0000

    refs #137 - Renamed the files and merged the split functionality into the script

---
+++ data_collection/ivol_split_files.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use diagnostics;
+
+use Getopt::Long;
+use Date::Manip;
+
+my $fh_read;
+my $fh_write;
+
+my $datafile;
+my $output_stem;
+my $replace_venue = 'DE';
+my $split_name    = 0;
+my $split_venue   = 1;
+my $split_date    = 1;
+
+GetOptions('datafile=s'      => \$datafile,
+           'output_stem=s'   => \$output_stem,
+           'replace_venue=s' => \$replace_venue,
+           'split_name!'     => \$split_name,
+           'split_venue!'    => \$split_venue,
+           'split_date!'     => \$split_date);
+
+
+if(!$datafile or !$output_stem) {
+    die("Usage: $0 --datafile <datafile> --output_stem <output_stem>\n");
+}
+
+
+open($fh_read, $datafile) or die("Cannot read $datafile\n");
+
+my $oldlabel = "";
+
+
+while(my $line = <$fh_read>) {
+    next if $line =~ /^$/;
+
+    my ($name, $venue, $datestr, @data) = split(",", $line);
+
+    $venue = $replace_venue if $venue =~ /\//;
+
+    my $date = UnixDate(ParseDate($datestr), '%Y%m%d');
+
+    my $label = ($split_venue ? "${venue}_" : "") .
+                ($split_date  ? "${date}_"   : "") .
+                ($split_name  ? "${name}"  : "");
+
+    if($label ne $oldlabel) {
+        $oldlabel = $label;
+
+        (my $filename = $output_stem) =~ s/XXXX/$label/g;
+
+        close($fh_write) if $fh_write;
+        open($fh_write, ">>" . $filename);
+    }
+
+    if($fh_write) {
+        print $fh_write $line;
+    } else {
+        print "Handle closed: " . $line if !$fh_write;
+    }
+}
+
+close($fh_write) if $fh_write;
