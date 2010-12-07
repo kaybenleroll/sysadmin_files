8767de78e0e2dd016d3a077627c142b416534951
Mick Cooney <mcooney@kaybensoft.com>
2010-12-07 03:24:14 +0000

    refs #137 - Added the ability to download multiple dates in inputs

--- data_collection/download_mx_data.pl
+++ data_collection/download_mx_data.pl
@@ -1,42 +1,66 @@
 #!/usr/bin/perl -w
 
+use strict;
+use warnings;
+use diagnostics;
+
 use LWP::UserAgent;
 use Date::Manip;
+use Getopt::Long;
 
 use strict;
 
 $|++;
 
-my $namefile = $ARGV[0];
+my $namefile;
+my $name       = 'XIU,ABX,SU,RY';
+my $start_date = '2009-01-01';
+my $end_date   = '2009-01-31';
 
-open(FILE, $namefile);
+GetOptions('namefile=s'    => \$namefile,
+           'name=s'        => \$name,
+           'start_date=s'  => \$start_date,
+           'end_date=s'    => \$end_date);
 
-my @symbols = <FILE>;
-chomp(@symbols);
+my @symbols;
 
-close(FILE);
+if($namefile) {
+    open(FILE, $namefile);
 
-my @start_dates = (ParseRecur('first day of every month in 2009'), ParseRecur('first day of every month in 2010') );
-my @end_dates   = (ParseRecur('last day of every month in 2009'),  ParseRecur('last day of every month in 2010') );
+    @symbols = <FILE>;
+    chomp(@symbols);
 
-print "Dates\n";
-print join(' ', @start_dates) . "\n";
-print join(' ', @end_dates) . "\n";
+    close(FILE);
+} elsif($name) {
+    @symbols = split(",", $name);
+} else {
+    die("Need to supply either a name or namefile parameter\n");
+}
+
+my @start = split(",", $start_date);
+my @end   = split(",", $end_date);
+
+if(@start != @end) {
+    die("The start and end lists need to be of equal length\n");
+}
 
 
 foreach my $symbol (@symbols) {
-    for(my $index = 0; $index < @start_dates; $index++) {
-        my $start_day   = UnixDate($start_dates[$index], '%d');
-        my $start_month = UnixDate($start_dates[$index], '%m');
-        my $start_year  = UnixDate($start_dates[$index], '%Y');
+    for(my $i = 0; $i < @start; $i++) {
+        my $start_date = $start[$i];
+        my $end_date   = $end[$i];
 
-        my $end_day     = UnixDate($end_dates[$index], '%d');
-        my $end_month   = UnixDate($end_dates[$index], '%m');
-        my $end_year    = UnixDate($end_dates[$index], '%Y');
+        my $start_day   = UnixDate($start_date, '%d');
+        my $start_month = UnixDate($start_date, '%m');
+        my $start_year  = UnixDate($start_date, '%Y');
+
+        my $end_day     = UnixDate($end_date, '%d');
+        my $end_month   = UnixDate($end_date, '%m');
+        my $end_year    = UnixDate($end_date, '%Y');
 
         my $url = "http://www.m-x.ca/nego_cotes_csv.php?symbol=${symbol}&lang_txt=en&jj=${start_day}&mm=${start_month}&aa=${start_year}&jjF=${end_day}&mmF=${end_month}&aaF=${end_year}";
 
-        my $outputfile = "mxoptiondata_${symbol}_" . UnixDate($start_dates[$index], '%Y%m%d') . '_' . UnixDate($end_dates[$index] , '%Y%m%d') . '.csv';
+        my $outputfile = "mxoptiondata_${symbol}_" . UnixDate($start_date, '%Y%m%d') . '_' . UnixDate($end_date , '%Y%m%d') . '.csv';
 
         print $url . "\t" . $outputfile . "\n";
 
@@ -51,7 +75,7 @@ foreach my $symbol (@symbols) {
         close(FILE);
 
         my $delay = int(rand(10) + 10);
-        print("Waiting for " . $delay . " secs\n\n");
+        print("Waiting for $delay secs\n\n");
         sleep($delay);
     }
 }
