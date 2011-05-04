cd34b87ad4e5f2099427b4c005589e6a799db6c5
Mick Cooney <mcooney@kaybensoft.com>
2011-05-04 12:32:40 +0100

    Added the eoddata.com data conversion script

--- conffiles/.Rprofile
+++ conffiles/.Rprofile
@@ -68,12 +68,12 @@ options(defaultPackages = c(getOption('defaultPackages'), 'ProjectTemplate'));
     napply <- function(names, fn) sapply(names, function(x) fn(get(x, pos = pos)))
     names <- ls(pos = pos, pattern = pattern)
 
-    obj.class <- napply(names, function(x) as.character(class(x))[1])
-    obj.mode <- napply(names, mode)
-    obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
-    obj.size <- napply(names, object.size)
+    obj.class      <- napply(names, function(x) as.character(class(x))[1])
+    obj.mode       <- napply(names, mode)
+    obj.type       <- ifelse(is.na(obj.class), obj.mode, obj.class)
+    obj.size       <- napply(names, object.size)
     obj.prettysize <- sapply(obj.size, function(r) prettyNum(r, big.mark = ",") )
-    obj.dim <- t(napply(names, function(x) as.numeric(dim(x))[1:2]))
+    obj.dim        <- t(napply(names, function(x) as.numeric(dim(x))[1:2]))
 
     vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
 
@@ -90,7 +90,7 @@ options(defaultPackages = c(getOption('defaultPackages'), 'ProjectTemplate'));
 
     if (head) { out <- head(out, n) }
 
-    out
+    return(out);
 }
 
 
---
+++ data_collection/eoddata_convert_csv.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use diagnostics;
+
+use Date::Manip;
+use Getopt::Long;
+
+use strict;
+
+my %month_code = ('F' => 'Jan',
+                  'G' => 'Feb',
+                  'H' => 'Mar',
+                  'J' => 'Apr',
+                  'K' => 'May',
+                  'M' => 'Jun',
+                  'N' => 'Jul',
+                  'Q' => 'Aug',
+                  'U' => 'Sep',
+                  'V' => 'Oct',
+                  'X' => 'Nov',
+                  'Z' => 'Dec');
+
+
+while(my $line = <>) {
+    chomp($line);
+
+    my ($fullsymbol, $date, $open, $high, $low, $close, $volume, $openinterest) = split(",", $line);
+
+    next if $line =~ '/^Symbol,/';
+
+    if($fullsymbol =~ /(.+)([A-Z])(\d+)/) {
+        my $symbol         = $1;
+        my $delivery       = $2;
+        my $year           = ($3 >= 90 ? 1900 : 2000) + $3;
+
+        if(!exists($month_code{$delivery})) { print STDERR "\nUnknown Delivery month code " . $delivery . "," . $year . ",($fullsymbol),$line\n"; }
+
+        my $delivery_month = $month_code{$2};
+        my $lasttrade      = "1-" . $delivery_month . "-" . $year;
+
+        print "$symbol,$date,$lasttrade,$open,$high,$low,$close,$volume,$openinterest\n";
+    }
+}
