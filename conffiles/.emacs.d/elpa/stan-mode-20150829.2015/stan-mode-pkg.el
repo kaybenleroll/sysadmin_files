84b623fdb5f25e1e4eeccfbc8c7422ac81b4af37
Mick Cooney <mickcooney@gmail.com>
2016-03-20 00:45:52 +0000

    Added the .emacs.d/ directory

---
+++ conffiles/.emacs.d/elpa/stan-mode-20150829.2015/stan-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "stan-mode" "20150829.2015" "Major mode for editing Stan files" 'nil :url "http://github.com/stan-dev/stan-mode" :keywords
+  '("languanges"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
